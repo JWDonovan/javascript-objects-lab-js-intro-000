6630030ec33d54c88b2f389dd52d969e3ead18a7
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 16:48:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  return object.assign(object, {key: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+}
